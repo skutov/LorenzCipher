c7db69d8d889c7a4f71b234f2f9dfe338160c4b1
Robb Gosset <skutov.robbie@gmail.com>
2013-12-13 03:28:13 +0000

    Started constructor and gave up for today

--- Lorenz SZ40/LorenzWheel.cpp
+++ Lorenz SZ40/LorenzWheel.cpp
@@ -1,6 +1,11 @@
 #include "LorenzWheel.h"
 
-LorenzWheel::LorenzWheel (int newWheelSize = 0)
+LorenzWheel::LorenzWheel (int newWheelSize = 0, bool *newPins)
 {
-
+	wheelPosition = 0;
+	wheelSize = newWheelSize;
+	for (int i = 0; i < wheelSize; i++)
+	{
+		wheelPins[i] = newPins[i];
+	}
 }
